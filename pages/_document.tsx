3a83c2337c7f630dedfd0e70c94e383788cb11b0
DavTech <dav-tech@outlook.com>
2022-02-26 14:45:48 +0100

    added comments for design files

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,10 @@
 import Document, { DocumentContext } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
+//######### Component Types ##################
+//######### Component Styles #################
+//######### Component ########################
+
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet();
