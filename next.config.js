5e62ebcb10987738635129c0649dddf2e31d8dd2
DavTech <dav-tech@outlook.com>
2022-12-25 22:37:51 +0100

    Bug: 🐛 fix .next file not created when running yarn dev

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
 };
 
-module.exports = {
-  ...nextConfig,
-  images: {
-    domains: ['127.0.0.1'],
-  },
-  distDir: 'build',
-};
+module.exports = nextConfig;
